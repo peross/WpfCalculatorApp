4e8478a88a665a6b6a509da5e40e0c25d91109fe
peross <pero.soldat@gmail.com>
2020-02-13 11:52:04 +0100

    Designed form and buttons.

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -24,5 +24,82 @@ namespace WpfCalculator
         {
             InitializeComponent();
         }
+
+        private void btnJedan_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "1";
+            else
+                txtEkran1.Text = txtEkran1.Text + "1";
+        }
+
+        private void btnDva_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "2";
+            else
+                txtEkran1.Text = txtEkran1.Text + "2";
+        }
+
+        private void btnTri_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "3";
+            else
+                txtEkran1.Text = txtEkran1.Text + "3";
+        }
+
+        private void btnCetiri_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "4";
+            else
+                txtEkran1.Text = txtEkran1.Text + "4";
+        }
+
+        private void btnPet_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "5";
+            else
+                txtEkran1.Text = txtEkran1.Text + "5";
+        }
+
+        private void btnSest_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "6";
+            else
+                txtEkran1.Text = txtEkran1.Text + "6";
+        }
+
+        private void btnSedam_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "7";
+            else
+                txtEkran1.Text = txtEkran1.Text + "7";
+        }
+
+        private void btnOsam_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "8";
+            else
+                txtEkran1.Text = txtEkran1.Text + "8";
+        }
+
+        private void btnDevet_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtEkran1.Text == "0" && txtEkran1.Text != null)
+                txtEkran1.Text = "9";
+            else
+                txtEkran1.Text = txtEkran1.Text + "9";
+        }
+
+        private void btnUlaz_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
